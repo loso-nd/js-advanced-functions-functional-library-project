09e1c6a63b36e425b71e5ae4a07f5db042cd1a84
Nahum <losond8@gmail.com>
2021-04-06 17:03:45 -0400

    commit tree 584e9a5f8eaf719de251d63309ab03f5c6ee3cb5

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function(testArr, alert) {
-      console.log(testArr, `one:`, testArr[0])
-      testArr.forEach( ele => {
-       console.log({one: "hi", two: array1[1], three: array1[2], four: array1[3]})
+    each: function(arr, someFn) {
+        console.log(arr, someFn)
 
-      })
+        let newArr; 
+        if(arr instanceof Array){ //Array.isArray(arr)
+            newArr = [...arr] //or newArr = arr.slice()
+        }
+        else {
+          newArr = Object.values(arr)
+        }
+       
+        for (let i = 0; i < newArr.length; i++) {
+          someFn(newArr[i]);
+          }
+
+          return arr
     },
+
 
     map: function() {
 
