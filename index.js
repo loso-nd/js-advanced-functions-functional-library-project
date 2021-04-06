971d1213d43256603052d9442e32c695e9cd1790
Nahum <losond8@gmail.com>
2021-04-06 14:46:01 -0400

    Nothing done.

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(testArr, alert) {
+      console.log(testArr, `one:`, testArr[0])
+      testArr.forEach( ele => {
+       console.log({one: "hi", two: array1[1], three: array1[2], four: array1[3]})
 
+      })
     },
 
     map: function() {
@@ -18,7 +22,7 @@ const fi = (function() {
 
     functions: function() {
 
-    },
+    }, 
 
 
   }
